d8f24ec7f88403e9b2890be6620838342e21ca39
Kyle <unixislife@gmail.com>
2019-05-05 22:18:00 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Check for existing Bashrc
+if [[ -f $HOME/.bashrc ]];then
+        mv $HOME/.bashrc $HOME/.bashrc.bak
+fi
+# Check for existing Vimrc
+if [[ -f $HOME/.vimrc ]];then
+        mv $HOME/.vimrc $HOME/.vimrc.bak
+fi
+# Check for existing Ansiblecfg
+if [[ -f $HOME/.ansible.cfg ]];then
+        mv $HOME/.ansiblecfg $HOME/.ansiblecfg.bak
+fi
 
 # dotfiles and vim
 ln -sv ~/dotfiles/.vimrc ~
@@ -10,4 +22,4 @@ ln -sv ~/dotfiles/.bashrc ~
 ln -sv ~/dotfiles/.ansible.cfg ~
 
 # tell yourself your done
-echo "Scripts in place, proceed to working"
+echo "All done"
