874da0b32cbde3a3704f1dd518d516a3fbb5979b
Kyle Walker <unixislife@gmail.com>
2021-12-28 23:56:55 -0800

    Adding ovirt req brew modules

--- mac_setup.sh
+++ mac_setup.sh
@@ -4,8 +4,11 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## Install core brew packages
-brew install bash vim tmux bat yamllint tig jq ansible
+brew install bash vim tmux bat yamllint tig jq ansible openjdk openssl libxml2
 
 ## Set Bash as primary shell (will require sudo password)
 sudo echo "/usr/local/bin/bash" >> /etc/shells
 chsh -s /usr/local/bin/bash
+
+## Set JDK symlink for java usage (will require sudo password)
+sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
