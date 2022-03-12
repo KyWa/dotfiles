d22efc91db0490b0f4d7aa43299fa1059021184b
Kyle Walker <unixislife@gmail.com>
2022-03-11 22:14:11 -0600

    fixing k8s prompt for ocp/okd

--- .k8sprompt.sh
+++ .k8sprompt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 k8s_context(){
-    CONTEXT=$(cat ~/.kube/config 2>/dev/null | grep -o 'namespace: [^/]*' | cut -d ' ' -f2)
+    CONTEXT=$(cat ~/.kube/config 2>/dev/null | grep -o 'current-context: [^/]*' | cut -d ' ' -f2)
 
     if [ -n "$CONTEXT" ];then
         echo "(k8s:${CONTEXT})"
