77dad755a32a15cd5076fe9e93f4fdc9aa663570
Kyle Walker <unixislife@gmail.com>
2025-07-25 10:17:52 -0500

    adding recent changes to .todo script

--- .todo.sh
+++ .todo.sh
@@ -5,23 +5,51 @@
 # If completed file, should it be cleaned up at a certain point?
 # Or if not, have it go remove/replace the FOLLOWUP field with another phrase?
 
+## INITIAL VARS
+FOLLOWUP_FILE="$HOME/.fup"
+TODO_FILE="$HOME/.todo"
+
+## Followups from engagement journals
 function todo_check_followups(){
     DOC_YEAR=`date +%Y`
     DOC_PATH=`find ~/engagements/Customers/*/Journals/ -iname "$DOC_YEAR-*.md"`
-    for x in $DOC_PATH;do
-        grep "FOLLOWUP" $x | sed "s/^[ \t]*//" | sed "s/-\ FOLLOWUP\ -\ //"
+    FUP_ID=0
+
+    if [[ -f "$FOLLOWUP_FILE" ]];then
+        echo "Followup file already exists."
+    else
+        touch "${FOLLOWUP_FILE}"
+    fi
+
+    for x in `grep FOLLOWUP $DOC_PATH`;do
+        echo "$FUP_ID: `echo $x | sed 's/^[ \t]*//' | sed 's/-\ FOLLOWUP\ -\ //'`" >> $FOLLOWUP_FILE
+        #echo "$FUP_ID: `echo $x | sed "s/^[ \t]*//" | sed "s/-\ FOLLOWUP\ -\ //"`" >> $FOLLOWUP_FILE
+        FUP_ID=$(expr $FUP_ID + 1)
     done
+    /usr/bin/cat $FOLLOWUP_FILE
+}
+
+function todo_remove_followups(){
+    echo "Removing Followup item"
+}
+
+function todo_manage_file(){
 }
 
 function todo(){
     if [[ -z $1 ]];then
         echo "What do you need todo?"
         read newtodo
-        echo $newtodo >> ~/.todo
+        echo $newtodo >> $TODO_FILE
     else
         case $1 in
             clean)
-                echo "" > ~/.todo
+                echo "" > $TODO_FILE
+                ;;
+            fup)
+                echo "Here are your Followup items:"
+                echo ""
+                todo_check_followups
                 ;;
             *)
                 echo "I have no idea what you want me to do"
